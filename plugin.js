9ce7a1b64ab9a5cb8b0cc7619b16645881d735c9
Yacine <Yacine-T@outlook.com>
2019-06-12 10:49:16 +0200

    ajout fonctionnalité dessiner et nettoyer

--- plugin.js
+++ plugin.js
@@ -1,20 +1,39 @@
 window.addEventListener("load", function(){
 
+	var clear = document.getElementById("clear");
+	var paint = false;
+	
+	can = document.querySelector("canvas");
+	context = can.getContext("2d");
+	can.addEventListener("mousedown", painting);
+	can.addEventListener("mouseup", finish);
+	can.addEventListener("click", draw);
+	clear.addEventListener("click", clearContent);
 
-var can = document.getElementById('canavs');
-context = can.getContext("2d");
+
+	function painting(){
+		paint = true;
+	}
+
+	function finish(){
+		paint = false;
+	}
+
+	function draw(e){
+		if (!paint) return;
+			context.strokeStyle="black";
+			context.lineWidth = 5;
+			context.lineCap = "round";
+			context.beginPath();
+			context.lineTo(e.clientX, e.clientY);
+			context.stroke();
+			context.closePath();
+	}
 
 
-function move(){
 
-	x = event.pageX;
-	y = event.pageY;
-}
-
-function clear(){
-			
+	function clearContent(){	
 		context.clearRect(0, 0, can.width, can.height);
-			
-			}
-			
-				});
+	}
+
+});
