fea85d1bed317b96b11fbfddd6c5e6cb94b9aaec
Yacine <Yacine-T@outlook.com>
2019-06-13 13:34:53 +0200

    ajout capacité dessin et choix des couleurs

--- plugin.js
+++ plugin.js
@@ -1,39 +1,66 @@
-window.addEventListener("load", function(){
+	window.addEventListener("load", function(){
 
-	var clear = document.getElementById("clear");
-	var paint = false;
-	
-	can = document.querySelector("canvas");
-	context = can.getContext("2d");
-	can.addEventListener("mousedown", painting);
-	can.addEventListener("mouseup", finish);
-	can.addEventListener("click", draw);
-	clear.addEventListener("click", clearContent);
+		var clear = document.getElementById("clear");
+		var paint = false;
+		var color = "black";
+		var bbutton = document.getElementById("blue");
+		var rbutton = document.getElementById("red");
+		var gbutton = document.getElementById("green");
+		var blbutton = document.getElementById("black")
 
+		can = document.querySelector("canvas");
+		context = can.getContext("2d");
+		can.addEventListener("mouseup", finish);
+		can.addEventListener("mousemove", draw);
+		clear.addEventListener("click", clearContent);
+		can.addEventListener("mousedown", painting);
+		bbutton.addEventListener("click", blue);
+		rbutton.addEventListener("click", red);
+		gbutton.addEventListener("click", green);
+		blbutton.addEventListener("click", black);
+		canvas.height = 300;
+		canvas.width = 300;
 
-	function painting(){
-		paint = true;
-	}
+			function clearContent(){	
+			context.clearRect(0, 0, can.width, can.height);
+		}
 
-	function finish(){
-		paint = false;
-	}
-
-	function draw(e){
-		if (!paint) return;
-			context.strokeStyle="black";
-			context.lineWidth = 5;
-			context.lineCap = "round";
+		function painting(){
+			paint = true;
 			context.beginPath();
-			context.lineTo(e.clientX, e.clientY);
-			context.stroke();
-			context.closePath();
+		}
+
+		function finish(){
+			paint = false;
+		}
+
+		function draw(e){
+			if (!paint) return 0;
+	var rect = e.target.getBoundingClientRect();
+      var x = e.clientX - rect.left;
+      var y = e.clientY - rect.top;
+				context.strokeStyle=color;
+				context.lineWidth = 5;
+				context.lineCap = "round";
+				context.lineTo(x, y);
+				context.stroke();
+		}
+
+	function blue() {
+		color = "blue";
+	}
+
+	function red() {
+		color = "red";
+	}
+
+	function green() {
+		color = "green";
+	}
+
+	function black(){
+		color = "black";
 	}
 
 
-
-	function clearContent(){	
-		context.clearRect(0, 0, can.width, can.height);
-	}
-
-});
+	});
